c32883008b5026e6facc62b517fcf63a556eae19
Santiago <santiagov801@gmail.com>
2022-11-04 23:00:32 -0500

    textures / lights added

--- src/script.js
+++ src/script.js
@@ -3,16 +3,12 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { PMREMGenerator } from 'three/src/extras/PMREMGenerator.js';
-import { ACESFilmicToneMapping, sRGBEncoding } from 'three';
+import { ACESFilmicToneMapping, Color, sRGBEncoding } from 'three';
 import {
   mergeBufferGeometries,
   ShapeBufferGeometry,
 } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { SimplexNoise } from 'simplex-noise';
-
-import alea from 'alea';
-
-// Debug
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -20,34 +16,6 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#FFEECC');
-
-//Plane
-// const mapWidth = 20;
-// const mapHeight = 20;
-// const tileGeometry = new THREE.BoxGeometry(1, 1, 1);
-
-// for (let i = 0; i < mapWidth; i++) {
-//   for (let j = 0; j < mapHeight; j++) {
-//     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-//     const tileMaterial = new THREE.MeshBasicMaterial({
-//       color: `#${randomColor}`,
-//     });
-//     const newTile = new THREE.Mesh(tileGeometry, tileMaterial);
-//     newTile.position.x = 1 * i;
-//     newTile.position.z = 1 * j;
-//     newTile.position.y = 1 * Math.random() * (Math.random() * 1);
-//     newTile.material.wireframe = true;
-//     scene.add(newTile);
-//   }
-// }
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
 
 /**
  * Sizes
@@ -103,16 +71,41 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.toneMapping = ACESFilmicToneMapping;
 renderer.outputEncoding = sRGBEncoding;
 renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 const FloatType = THREE.FloatType;
 let envmap;
-const MAX_HEIGHT = 25;
+const MAX_HEIGHT = 20;
+const STONE_HEIGHT = MAX_HEIGHT * 0.8;
+const DIRT_HEIGHT = MAX_HEIGHT * 0.7;
+const GRASS_HEIGHT = MAX_HEIGHT * 0.5;
+const SAND_HEIGHT = MAX_HEIGHT * 0.3;
+const DIRT2_HEIGHT = MAX_HEIGHT * 0;
+
 const asyncloading = async () => {
   let pmrem = new PMREMGenerator(renderer);
   let envmapTexture = await new RGBELoader()
     .setDataType(FloatType)
     .loadAsync('/assets/envmap.hdr');
   envmap = pmrem.fromEquirectangular(envmapTexture).texture;
+
+  let textures = {
+    dirt: await new THREE.TextureLoader().loadAsync('assets/textures/dirt.png'),
+    dirt2: await new THREE.TextureLoader().loadAsync(
+      'assets/textures/dirt2.jpg'
+    ),
+    grass: await new THREE.TextureLoader().loadAsync(
+      'assets/textures/grass.jpg'
+    ),
+    sand: await new THREE.TextureLoader().loadAsync('assets/textures/sand.jpg'),
+    water: await new THREE.TextureLoader().loadAsync(
+      'assets/textures/water.jpg'
+    ),
+    stone: await new THREE.TextureLoader().loadAsync(
+      'assets/textures/stone.png'
+    ),
+  };
 
   const simplex = new SimplexNoise();
 
@@ -125,32 +118,48 @@ const asyncloading = async () => {
       if (noise < 0) {
         noise = Math.abs(noise);
       }
-      noise = Math.pow(noise, 1.5);
+      noise = Math.pow(noise, 0.5);
 
       makeHex(noise * MAX_HEIGHT, position);
     }
   }
 
-  let hexagonMesh = new THREE.Mesh(
-    hexagonGeometries,
-    new THREE.MeshStandardMaterial({
-      envMap: envmap,
-      flatShading: true,
-    })
-  );
+  let stoneMesh = hexMesh(stoneGeo, textures.stone);
+  let grassMesh = hexMesh(grassGeo, textures.grass);
+  let dirt2Mesh = hexMesh(dirt2Geo, textures.dirt2);
+  let dirtMesh = hexMesh(dirtGeo, textures.dirt);
+  let sandMesh = hexMesh(sandGeo, textures.sand);
+  scene.add(stoneMesh, grassMesh, dirt2Mesh, dirtMesh, sandMesh);
 
-  scene.add(hexagonMesh);
   renderer.setAnimationLoop(() => {
     renderer.render(scene, camera);
   });
 };
 asyncloading();
+const light = new THREE.PointLight(
+  new Color('#FFCBBE').convertSRGBToLinear().convertSRGBToLinear(),
+  80,
+  200
+);
+light.position.set(10, 20, 10);
+
+light.castShadow = true;
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+scene.add(light);
 
 function tileToPosition(tileX, tileY) {
   return new THREE.Vector2((tileX + (tileY % 2) * 0.5) * 1.77, tileY * 1.535);
 }
 
-let hexagonGeometries = new THREE.BoxGeometry(0, 0, 0);
+let stoneGeo = new THREE.BoxGeometry(0, 0, 0);
+let dirtGeo = new THREE.BoxGeometry(0, 0, 0);
+let dirt2Geo = new THREE.BoxGeometry(0, 0, 0);
+let sandGeo = new THREE.BoxGeometry(0, 0, 0);
+let grassGeo = new THREE.BoxGeometry(0, 0, 0);
+
 const hexGeometry = (height, position) => {
   let geo = new THREE.CylinderGeometry(1, 1, height, 6, 1, false);
   geo.translate(position.x, height * 0.5, position.y);
@@ -160,7 +169,30 @@ const hexGeometry = (height, position) => {
 
 function makeHex(height, position) {
   let geo = hexGeometry(height, position);
-  hexagonGeometries = mergeBufferGeometries([hexagonGeometries, geo]);
+  if (height > STONE_HEIGHT) {
+    stoneGeo = mergeBufferGeometries([geo, stoneGeo]);
+  } else if (height > DIRT_HEIGHT) {
+    dirtGeo = mergeBufferGeometries([geo, dirtGeo]);
+  } else if (height > GRASS_HEIGHT) {
+    grassGeo = mergeBufferGeometries([geo, grassGeo]);
+  } else if (height > SAND_HEIGHT) {
+    sandGeo = mergeBufferGeometries([geo, sandGeo]);
+  } else if (height > DIRT2_HEIGHT) {
+    dirt2Geo = mergeBufferGeometries([geo, dirt2Geo]);
+  }
+}
+
+function hexMesh(geo, map) {
+  let mat = new THREE.MeshPhysicalMaterial({
+    envMap: envmap,
+    envMapIntensity: 0.135,
+    flatShading: true,
+    map,
+  });
+  let mesh = new THREE.Mesh(geo, mat);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  return mesh;
 }
 
 const clock = new THREE.Clock();
